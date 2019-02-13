6fe99133b45c2fb7c2d1223023b8b9db561ea220
fullstackmommy <nicole.budiman@gmail.com>
2019-02-14 05:29:45 +0800

    Add To Do List

--- script.js
+++ script.js
@@ -22,21 +22,28 @@ function addTask(inputTask) {
 	const ul = document.querySelector('ul');
 	const li = document.createElement('li');
 
-	const newInput = document.createElement('span');
-	const newTrash = document.createElement('span');
+	const newInput = document.createElement('div');
+	const newTrash = document.createElement('div');
 	newTrash.classList.add("trash");
 
 	newInput.textContent = inputTask;
 	newInput.addEventListener('click' , (event) => {
+		newInput.classList.add('animated', 'bounce');
 		newInput.classList.toggle("done");
+		if (!newInput.classList.value.includes("done"))
+			newInput.classList.remove('animated', 'bounce');
 	})
 
 	li.append(newInput, newTrash);
 	ul.appendChild(li);
 
-	newTrash.textContent = " Remove";
+	//newTrash.textContent = "Remove";
+	newTrash.innerHTML = "<i class=\"fas fa-trash-alt\"></i>";
 	newTrash.addEventListener('click', (event) => {
-		newTrash.parentNode.remove();
+		newTrash.parentNode.classList.add('animated', 'bounceOutLeft');
+		newTrash.parentNode.addEventListener('animationend', () => {
+			newTrash.parentNode.remove();
+		})
 	})
 }
 
